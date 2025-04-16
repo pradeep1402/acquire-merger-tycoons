fdc6417aeabcbd0ff1507e775ef32193d95b2a0a
Pradeep Bisht <107491133+pradeep1402@users.noreply.github.com>
2025-04-16 17:58:14 +0530

    [#10] | [malli/bisht] | adding board, pile properties and getBoard

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,10 +1,20 @@
+import { Tile } from "./tile.ts";
+
 class Acquire {
   id: string;
   players: { id: number; name: string }[];
+  pile: string[];
+  board: Tile[];
 
-  constructor() {
+  constructor(tiles: string[]) {
     this.id = "1";
     this.players = [{ id: 1, name: "krishnanand" }];
+    this.pile = tiles;
+    this.board = tiles.map((tile: string): Tile => new Tile(tile));
+  }
+
+  getBoard(): Tile[] {
+    return this.board;
   }
 }
 
--- test/app_test.ts
+++ test/app_test.ts
@@ -5,7 +5,7 @@ import { Acquire } from "../src/models/game.ts";
 
 describe("APP: acquire/game", () => {
   it("should set session id and player name", async () => {
-    await (await createApp(new Acquire()).request("/")).text();
+    await (await createApp(new Acquire(["1A", "2A"])).request("/")).text();
 
     assertEquals(1, 1);
   });
