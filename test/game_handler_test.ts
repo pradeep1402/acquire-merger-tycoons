b22af670d87b4e16303861903a55b3c4e0df0885
Pradeep Bisht <107491133+pradeep1402@users.noreply.github.com>
2025-04-16 18:53:15 +0530

    #10 | [malli/bisht] | added handler for gameboard, route and testcase

--- test/game_handler_test.ts
+++ test/game_handler_test.ts
@@ -15,3 +15,32 @@ describe("App: acquire/players", () => {
     assertEquals(res.status, 200);
   });
 });
+
+describe("App: acquire/gameboard", () => {
+  it("should return the board array", async () => {
+    const players: Player[] = [{ id: 2, name: "krishnanand" }];
+
+    const acquire = new Acquire(["1A", "2A"], players);
+    const app = createApp(acquire);
+    const res = await app.request("acquire/gameboard");
+    const board = [
+      {
+        label: "1A",
+        isIndependent: false,
+        isDead: false,
+        hotel: null,
+        isOccupied: false,
+      },
+      {
+        label: "2A",
+        isIndependent: false,
+        isDead: false,
+        hotel: null,
+        isOccupied: false,
+      },
+    ];
+
+    assertEquals(await res.json(), board);
+    assertEquals(res.status, 200);
+  });
+});
