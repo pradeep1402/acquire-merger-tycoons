7de2cc705e9b36134861b71fb648e9c0596d102a
Mohammadsiddiqueps <126324832+Mohammadsiddiqueps@users.noreply.github.com>
2025-04-17 19:30:57 +0530

    #72 [sid,kri] implemented the basic html and styles for the lobby page with dummy data

---
+++ public/script/lobby.js
@@ -0,0 +1,26 @@
+const applyPlayerTemplate = (player) => {
+  const template = document.getElementById("lobby-template");
+  const clone = template.content.cloneNode(true);
+
+  clone
+    .getElementById("player-avatar")
+    .setAttribute("src", "/images/avatars/avatar3.jpeg");
+  clone.getElementById("player-name").textContent = player;
+
+  return clone;
+};
+
+const renderWaitingList = () => {
+  const players = ["boy", "girl", "pragna"];
+  const playerList = document.getElementById("player-list");
+
+  players.forEach((p) => {
+    playerList.appendChild(applyPlayerTemplate(p));
+  });
+};
+
+const main = () => {
+  renderWaitingList();
+};
+
+globalThis.onload = main;
--- test/player_test.ts
+++ test/player_test.ts
@@ -6,7 +6,7 @@ describe("Player model", () => {
   it("testing constructor", () => {
     const tiles: string[] = ["1a", "2a", "3a", "4a", "3b", "6a"];
     const player = new Player("Sudheer", tiles);
-    const person = player.toJSON()
+    const person = player.toJSON();
     assertEquals(person.name, "Sudheer");
     assertEquals(person.tiles, tiles);
     assertEquals(person.cash, 6000);
