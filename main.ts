45a0f94545df4c6c696fb5d9d00033acf8ade388
malleswaribatta <183718953+malleswaribatta@users.noreply.github.com>
2025-04-17 14:04:58 +0530

    [#3] | [malli/likhi] | adding authentication

--- main.ts
+++ main.ts
@@ -4,8 +4,9 @@ import { Acquire } from "./src/models/game.ts";
 const main = async () => {
   const tiles = JSON.parse(await Deno.readTextFile("./src/data/tiles.json"));
   const players: string[] = ["Adi", "Krishna", "Siddhik"];
-  const acquire = new Acquire(tiles, players);
-  const app = createApp(acquire);
+  const acquire: Acquire = new Acquire(tiles, players);
+  const sessions: Set<string> = new Set();
+  const app = createApp(acquire, sessions);
 
   Deno.serve({ port: 2616 }, app.fetch);
 };
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,90 @@
 import { Context, Hono, Next } from "hono";
 import { serveStatic } from "hono/deno";
 import { Acquire } from "./models/game.ts";
-import { serveGameBoard, servePlayers } from "./handlers/game_handler.ts";
+import {
+  handleLogin,
+  serveGameBoard,
+  servePlayers,
+} from "./handlers/game_handler.ts";
+import { getCookie } from "hono/cookie";
 
 const setContext =
-  (acquire: Acquire) => async (context: Context, next: Next) => {
+  (acquire: Acquire, sessions: Set<string>) =>
+  async (context: Context, next: Next) => {
+    context.set("sessions", sessions);
     context.set("acquire", acquire);
     await next();
   };
 
-export const createApp = (acquire: Acquire) => {
-  const app = new Hono();
+// const authenticateUser = async (ctx: Context, next: Next) => {
+//   const session = getCookie(ctx, "sessionId");
+//   const url = new URL(ctx.req.url);
 
-  app.get("*", serveStatic({ root: "./public" }));
-  app.use(setContext(acquire));
-  app.get("/acquire/gameboard", serveGameBoard);
-  app.get("/acquire/players", servePlayers);
+//   if (!session) {
+//     return ctx.redirect("/pages/login.html", 303);
+//   }
+
+//   await next();
+// };
+
+const ensureGuest = async (c: Context, next: Next) => {
+  const sessionId = getCookie(c, "sessionId");
+  const sessions = c.get("sessions");
+
+  if (sessionId && sessions.has(sessionId)) {
+    return c.redirect("/", 303);
+  }
+
+  return await next();
+};
+
+const createGuestRoutes = () => {
+  const guestRoutes = new Hono();
+  guestRoutes
+    .use("/login", ensureGuest)
+    .get("/login", serveStatic({ path: "./public/login.html" }))
+    .post(handleLogin);
+  return guestRoutes;
+};
+
+const ensureAuthenticated = async (c: Context, next: Next) => {
+  const sessionId = getCookie(c, "sessionId");
+  const sessions = c.get("sessions");
+
+  if (!sessions.has(sessionId)) {
+    return c.redirect("/login.html", 303);
+  }
+  return await next();
+};
+
+const createAuthenticatedRoutes = () => {
+  const authenticatedRoutes = new Hono();
+
+  authenticatedRoutes.use(ensureAuthenticated);
+
+  authenticatedRoutes.get("/*", serveStatic({ root: "./public" }));
+  return authenticatedRoutes;
+};
+
+export const createApp = (acquire: Acquire, sessions: Set<string>) => {
+  const guestRoutes = createGuestRoutes();
+  const authenticatedRoutes = createAuthenticatedRoutes();
+  const app = new Hono();
+   
+  app.use(setContext(acquire, sessions));
+  app.route("/", guestRoutes);
+  app.route("/", authenticatedRoutes);
   return app;
 };
+
+// export const createApp = (acquire: Acquire, sessions: Set<string>) => {
+//   const app = new Hono();
+
+//   app.use(setContext(acquire, sessions));
+//   app.use("*", authenticateUser);
+//   app.post("/login", handleLogin);
+//   app.get("/acquire/gameboard", serveGameBoard);
+//   app.get("/acquire/players", servePlayers);
+//   app.get("*", serveStatic({ root: "./public" }));
+//   return app;
+// };
