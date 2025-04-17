2265a100d676181d4758716b6bbd4bc4b465b134
Pradeep Bisht <107491133+pradeep1402@users.noreply.github.com>
2025-04-17 09:59:27 +0530

    #10 | [malli/bisht] | imported all tiles names from data/tiles.json

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 import { createApp } from "./src/app.ts";
 import { Acquire, Player } from "./src/models/game.ts";
 
-const main = () => {
+const main = async () => {
+  const tiles = JSON.parse(await Deno.readTextFile("./src/data/tiles.json"));
   const players: Player[] = [{ id: 2, name: "krishnanand" }];
-  const acquire = new Acquire(["1A", "2A"], players);
+  const acquire = new Acquire(tiles, players);
   const app = createApp(acquire);
 
   Deno.serve({ port: 2616 }, app.fetch);
